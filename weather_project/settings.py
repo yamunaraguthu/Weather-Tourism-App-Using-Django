6c75d0bf6539ff37428145135f3e502b8df78904
Yamuna Raguthu <yamunaraguthu2003@gmail.com>
2026-04-14 12:00:05 +0530

    Update settings.py

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-mkrfs40fe6823y8b@k(!s3fd_-ujl3_mu_rm@qwpp71tsvk!&0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
